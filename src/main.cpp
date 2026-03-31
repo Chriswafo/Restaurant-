76c205de6d6de331f7ed64794416be31a3e324a1
Chriswafo <chriswafo16@gmail.com>
2026-03-31 21:40:52 +0100

    Ajout de la fonctionnalité de gestion des plats

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,32 @@
 #include <locale>
 
 using namespace std;
+class Plat{
+private:
+    string nom;
+    float prix;
+public:
+    Plat(string n,float p):nom(n),prix(p){}
+    Plat():nom(""),prix(0.0){}
+    string getnom()const{
+      return nom;
+    }
+    float getprix()const {
+      return prix;
+    }
+    void setnom(string n){
+      nom=n;
+    }
+    void setprix(float p){
+      if(p>0)
+        prix=p;
+      else
+        prix=0;
+    }
+    void afficherPlat(){
+       cout<<"le plat "<<nom<<" coute "<<prix<<" £"<<endl;
+    }
+};
 int main()
 {
    setlocale(LC_ALL,"");
